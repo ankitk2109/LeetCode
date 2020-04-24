40461685731c564dde0ede3440fbc9a0b79250ff
ankitk2109 <ankitk2109@gmail.com>
2020-04-24 11:42:05 +0100

    Best Time to Buy and Sell Stock

--- 30DaysOfCode/5. BestTimeToBuyAndSellStock.py
+++ 30DaysOfCode/5. BestTimeToBuyAndSellStock.py
@@ -1,7 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        
         max_profit = 0
+
         for i in range(len(prices)-1):
+            # Finding positive difference between pairs
             if(prices[i+1] - prices[i] > 0):
                 max_profit += prices[i+1] - prices[i]
+
         return(max_profit)

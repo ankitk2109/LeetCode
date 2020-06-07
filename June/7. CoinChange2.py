6d09465d4c2ce53d701ab5db80755d86a5d45f41
ankitk2109 <ankitk2109@gmail.com>
2020-06-07 10:24:11 +0100

    Create 7. CoinChange2.py

---
+++ June/7. CoinChange2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        change_method_count = [1] + [ 0 for _ in range(amount)]
+        
+        # make change with current coin, from small coin to large coin
+        for cur_coin in coins:
+            
+            # update change method count from small amount to large amount
+            for small_amount in range(cur_coin, amount+1):
+                
+                # current small amount can make changed with current coin
+                change_method_count[small_amount] += change_method_count[small_amount - cur_coin]
+                
+        return change_method_count[amount]

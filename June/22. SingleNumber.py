ece9422670e24a88e27f601b1811ea648188d550
Ankit Kumar <ankit@ucdconnect.ie>
2020-06-22 09:21:49 +0100

    Create 22. SingleNumber.py

---
+++ June/22. SingleNumber.py
@@ -0,0 +1,6 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        for k in counter:
+            if counter[k]==1:
+                return(k)

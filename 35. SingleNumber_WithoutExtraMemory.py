a6f748503c040e5f47919a7cd2118a9fa0ca983f
Ankit Kumar <ankitk2109@gmail.com>
2020-06-23 14:00:58 +0100

    Update 35. SingleNumber_WithoutExtraMemory.py

--- 35. SingleNumber_WithoutExtraMemory.py
+++ 35. SingleNumber_WithoutExtraMemory.py
@@ -1,3 +1,5 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:      
-        return (sum(list(set(nums)))*3-sum(nums))//2
+    def singleNumber(self, nums: List[int]) -> int: 
+
+        ans = (sum(list(set(nums)))*3-sum(nums))//2
+        return ans

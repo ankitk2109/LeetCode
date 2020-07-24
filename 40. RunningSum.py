2252b6e816a3f5f52f4e73ebc7aa99e05766c081
Ankit Kumar <ankitk2109@gmail.com>
2020-07-24 10:59:48 +0100

    Running Sum

---
+++ 40. RunningSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        cur_sum = 0
+        
+        for i in range(len(nums)):
+            cur_sum += nums[i]
+            nums[i] = cur_sum
+            
+        return(nums)
+            
